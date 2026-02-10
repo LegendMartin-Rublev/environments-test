c347db235ecb6a97f91b0c78495592f970c02222
martin-rublev <martin.rublev@l1.com>
2026-02-10 13:51:25 +0200

    test signed commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="min-h-screen bg-gray-200 pt-4">
-    <h1 class="text-4xl font-bold text-center text-gray-900 mb-2 font-mono">Oddspedia Environments List</h1>
+    <h1 class="text-4xl font-bold text-center text-gray-900 mb-2 font-mono">Oddspedia Environments</h1>
     
     <p v-if="currentUserName" class="text-xl text-center text-gray-700 mb-4">Hi {{ currentUserName }}</p>
     
